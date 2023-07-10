a9c859ca5aa318de189278e1b46102cb18b9300e
greatooau <pajaroazulx@gmail.com>
2023-07-10 12:30:05 -0600

    feat(AuroraGuard.Core): Models and Interfaces
    User and Credential models were added.
    Interface for Unit of Work was created

---
+++ AuroraGuard.Core/Models/Credential.cs
@@ -0,0 +1,11 @@
+﻿namespace AuroraGuard.Core.Models;
+
+public class Credential
+{
+	public string Id { get; set; } = null!;
+	public string AccessUser { get; set; } = null!;
+	public string AccessPassword { get; set; } = null!;
+	public string UserId { get; set; } = null!;
+	public DateTime ModifiedAt { get; set; }
+	public DateTime CreatedAt { get; set; }
+}
---
+++ AuroraGuard.Core/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuroraGuard.Core.Models;
+
+public class User
+{
+	public string Id { get; set; } = null!;
+	public string Password { get; set; } = null!;
+	public string Salt { get; set; } = null!;
+	public string Name { get; set; } = null!;
+	public DateTime CreatedAt { get; set; }
+	public DateTime UpdatedAt { get; set; }
+}
