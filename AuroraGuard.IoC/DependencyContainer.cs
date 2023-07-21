23156ff7778588b787852387575d2e828f25ad98
greatooau <pajaroazulx@gmail.com>
2023-07-21 12:27:13 -0600

    feat(AuroraGuard.IoC): inversion of control project added to isolate app dependencies

---
+++ AuroraGuard.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using AuroraGuard.DataAccess;
+using AuroraGuard.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuroraGuard.IoC;
+
+public static class DependencyContainer
+{
+	public static IServiceCollection AddAuroraGuardDependencies(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDataAccess(configuration);
+		services.AddAuroraGuardServices();
+		return services;
+	}
+}
