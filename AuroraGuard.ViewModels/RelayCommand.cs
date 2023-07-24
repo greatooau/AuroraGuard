40057361e844a1de17a16e17f46bd7c78e72334a
greatooau <pajaroazulx@gmail.com>
2023-07-24 16:54:13 -0600

    feat(AuroraGuard.ViewModels): Added Login View model initial logic

---
+++ AuroraGuard.ViewModels/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace AuroraGuard.ViewModels;
+
+public class RelayCommand : ICommand
+{
+	private readonly Action<object> _execute;
+	private readonly Predicate<object?>? _canExecute;
+
+	public RelayCommand(Action<object> execute, Predicate<object?>? canExecute = null)
+	{
+		_execute = execute;
+		_canExecute = canExecute;
+	}
+	
+	[DebuggerStepThrough]
+	public bool CanExecute(object? parameter)
+	{
+		return _canExecute == null || _canExecute(parameter);
+	}
+
+	public void Execute(object? parameter)
+	{
+		_execute(parameter!);
+	}
+
+	public event EventHandler? CanExecuteChanged;
+}
