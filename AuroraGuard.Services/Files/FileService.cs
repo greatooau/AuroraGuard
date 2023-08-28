1834fb6a5f009b437cafcc0172c2c9bc3b953464
greatooau <pajaroazulx@gmail.com>
2023-08-28 09:12:57 -0600

    feat(Services): File Service and abstraction added

---
+++ AuroraGuard.Services/Files/FileService.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Abstractions;
+using AuroraGuard.Services.Dialog;
+
+namespace AuroraGuard.Services.Files;
+
+public class FileService : IFileService
+{
+	private readonly IDialogService _dialogService;
+	private readonly IFile _file;
+
+	public FileService(IDialogService dialogService, IFile file)
+	{
+		_dialogService = dialogService;
+		_file = file;
+	}
+
+	public bool CreateFile(string path)
+	{
+		try
+		{
+			_file.Create(path);
+			return true;
+		}
+		catch (Exception e)
+		{
+			_dialogService.ShowError(e, "File creation Error");
+			return false;
+		}
+	}
+	
+
+	public async Task<bool> WriteBytesAsync(string path, byte[] bytes)
+	{
+		try
+		{
+			if (!_file.Exists(path))
+				throw new FileNotFoundException($"File {path} not found", path);
+
+			await _file.WriteAllBytesAsync(path, bytes);
+			return true;
+		}
+		catch (FileNotFoundException e)
+		{
+			_dialogService.ShowError(e, e.Message);
+			return false;
+		}
+	}
+}
---
+++ AuroraGuard.Services/Files/IFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace AuroraGuard.Services.Files;
+
+public interface IFileService
+{
+	public bool CreateFile(string path);
+	public Task<bool> WriteBytesAsync(string path, byte[] bytes);
+}
