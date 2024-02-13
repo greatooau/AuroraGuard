e6288ba3e5fe15bbdad9eb43e5c645b7fc94e039
greatooau <pajaroazulx@gmail.com>
2024-02-13 16:15:38 -0600

    feat(user interface): clipboard service implementation

---
+++ AuroraGuard.UserInterface.WPF/Services/ClipboardService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+using AuroraGuard.Core.Interfaces.Services;
+
+namespace AuroraGuard.UserInterface.WPF.Services;
+
+public class ClipboardService : IClipboardService
+{
+    public void CopyText(string text) => Clipboard.SetText(text);
+}
