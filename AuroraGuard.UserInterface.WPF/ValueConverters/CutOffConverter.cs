205e54c9fe56a195d98d314525bad7ca9f2684a6
greatooau <pajaroazulx@gmail.com>
2024-02-13 16:16:46 -0600

    update(user interface): handled empty string in nullable field

--- AuroraGuard.UserInterface.WPF/ValueConverters/CutOffConverter.cs
+++ AuroraGuard.UserInterface.WPF/ValueConverters/CutOffConverter.cs
@@ -4,6 +4,7 @@ using System.Windows.Data;
 
 namespace AuroraGuard.UserInterface.WPF.ValueConverters;
 
+[ValueConversion(typeof(int), typeof(bool))]
 public class CutOffConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => value is not null && (int)value > Cutoff;
---
+++ AuroraGuard.UserInterface.WPF/ValueConverters/EmptyStringToNullConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AuroraGuard.UserInterface.WPF.ValueConverters;
+
+public class EmptyStringToNullConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null) return null;
+
+        var text = (string)value;
+
+        return text == "" ? null : text;
+    }
+}
